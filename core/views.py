aa0359adfe9dd790281a23f66487200e83d75241
Eden Iyanda <edeniyanda@gmail.com>
2024-08-31 19:55:15 +0100

    UPdate: Worked on the  homr pagr to display items and price

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    return render(request, "core/home.html", {})
+    products = Product.objects.all()
+
+    return render(request, "core/home.html", { "products" : products})
 
 def cart(request):
     return render(request, "core/cart.html", {})
